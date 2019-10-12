d0df92c993ce980693b8add4a58120dea29cb5c8
Zchule <zulemavp94@gmail.com>
2019-10-12 01:45:07 -0500

    Eslint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../assets/style/App.scss'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
